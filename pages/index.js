df1b6b7265f91455064c06f90c372c0eeeffac0b
GHAndrade25 <gui-henrique08@hotmail.com>
2025-03-30 14:24:36 +0000

    adiciona `.prettierignore`

--- pages/index.js
+++ pages/index.js
@@ -2,12 +2,4 @@ function Home() {
   return <h1>Primeiro site ta sendo criado já</h1>;
 }
 
-function teste() {
-  console.log9("teste");
-}
-
-function teste2() {
-  console.log("indentação errada");
-}
-
 export default Home;
