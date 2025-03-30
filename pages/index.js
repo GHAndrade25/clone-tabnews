53d067c8972a28ecf2d2e0d3a8eeb8772be22316
GHAndrade25 <gui-henrique08@hotmail.com>
2025-03-30 13:35:28 +0000

    adicionad scripts `lint:check`e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,13 @@
-function Home () {
-    return <h1>Primeiro site ta sendo criado já</h1>
+function Home() {
+  return <h1>Primeiro site ta sendo criado já</h1>;
+}
+
+function teste() {
+  console.log9("teste");
+}
+
+function teste2() {
+  console.log("indentação errada");
 }
 
 export default Home;
